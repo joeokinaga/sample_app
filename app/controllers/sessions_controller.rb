ce66b9e434065744d60a160583b646f8140abfc3
okinagajoe <joeokinaga0921@gmail.com>
2025-07-29 19:56:41 +0200

    add a function that login when a user sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-        reset_session
       reset_session      # ログインの直前に必ずこれを書くこと
       log_in user
       redirect_to user

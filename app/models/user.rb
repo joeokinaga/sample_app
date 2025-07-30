9126f76da199d7575152d97c90f831dcd2ce0dd8
okinagajoe <joeokinaga0921@gmail.com>
2025-07-30 16:57:25 +0200

    add forget function

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,5 +29,10 @@ class User < ApplicationRecord
     # 渡されたトークンがダイジェストと一致したらtrueを返す
     def authenticated?(remember_token)
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end    
+    end   
+
+    # ユーザーのログイン情報を破棄する
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end

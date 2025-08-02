0737a9d0bcdd5b39bd3e7b50ed07756669b57ddf
okinagajoe <joeokinaga0921@gmail.com>
2025-08-02 13:36:37 +0200

    implement users page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,27 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links with login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+  end
+
+  test "layout links without login" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", login_path
+  end
+
+  test "common layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2

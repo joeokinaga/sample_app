e55bd560984488fe4dd8acb80eaefc61b06d3a0f
okinagajoe <joeokinaga0921@gmail.com>
2025-08-20 13:14:05 +0900

    fix user.rb (to easy to see private method)

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,14 +59,14 @@ class User < ApplicationRecord
     
     private
 
-    # メールアドレスをすべて小文字にする
-    def downcase_email
-      email.downcase!
-    end
+        # メールアドレスをすべて小文字にする
+        def downcase_email
+            email.downcase!
+        end
 
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+        # 有効化トークンとダイジェストを作成および代入する
+        def create_activation_digest
+            self.activation_token  = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end

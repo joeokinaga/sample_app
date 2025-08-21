178a7406c3bbaa2279ddf91d9ca3f0ec84221c53
okinagajoe <joeokinaga0921@gmail.com>
2025-08-21 16:45:20 +0900

    add stats view and follow, unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
+  # get 'password_resets/new'
+  # get 'password_resets/edit'
   root   "static_pages#home"
-  get    "/help", to: "static_pages#help"
-  get    "/about", to: "static_pages#about"
+  get    "/help",    to: "static_pages#help"
+  get    "/about",   to: "static_pages#about"
   get    "/contact", to: "static_pages#contact"
   get    "/privacy", to: "static_pages#privacy"
-  get    "/signup", to: "users#new"
+  get    "/signup",  to: "users#new"
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  # resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
   # root "application#hello"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

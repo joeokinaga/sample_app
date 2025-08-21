8f640d0e1e80f5368feee071929ac5050124a07c
okinagajoe <joeokinaga0921@gmail.com>
2025-08-21 13:30:45 +0900

    implement attaching an image

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -71,3 +71,20 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "1 micropost", response.body
   end
 end
+
+
+
+class ImageUploadTest < MicropostsInterface
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type="file"][name="micropost[image]"]'
+  end
+
+  # test "should be able to attach an image" do
+  #   cont = "This micropost really ties the room together."
+  #   img  = fixture_file_upload('kitten.jpg', 'image/jpeg')
+  #   post microposts_path, params: { micropost: { content: cont, image: img } }
+  #   assert @micropost.image.attached?
+  # end
+end
